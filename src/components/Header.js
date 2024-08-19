b4331e6900bc34044b61927f84ebade25feffde5
Caroline <bancroftwanjiru10000@gmail.com>
2024-08-19 20:44:11 +0300

    submitted assignment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,11 @@
-import React from "react";
+import React from 'react';
 
-function Header({onDarkModeClick, isDarkMode}){
+const Header = ({ isDarkMode, onDarkModeClick }) => (
+  <header>
+    <button onClick={onDarkModeClick}>
+      {isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+    </button>
+  </header>
+);
 
-    return(
-        <div className={"App " + (isDarkMode ? "dark" : "light")}>
-     <header>
-        <h2>Shopster</h2>
-     <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-        </header>
-        </div>
-    )
-}
 export default Header;
